e4e83924b7fffec56eb6839974b19c434c2bab6b
vishalnaikr <naikv7403@gmail.com>
2023-06-01 14:04:57 +0530

    first

---
+++ Circle.java
@@ -0,0 +1,14 @@
+class Circle
+{
+static void area()
+{
+  double pi=3.142;
+  int b=5;
+ double r=pi*b*b;
+ System.out.println(r);
+ }
+ public static void main(String[] args)
+ {
+ area();
+ }
+ }
